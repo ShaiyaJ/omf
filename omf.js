71de04b944072a46a07d8952a7f037902efb9b6f
Snishi <93013772+ShaiyaJ@users.noreply.github.com>
2025-07-16 00:40:51 +0100

    code style and clarity changes

--- omf.js
+++ omf.js
@@ -3,9 +3,9 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
     let extendTagClass = extendTagName === null ? HTMLElement : document.createElement(extendTagName).constructor;
 
     // Create dynamic object
-    const dynamicExtend = class DYNEX extends extendTagClass {
-        static fetchRan = false;  // Updates to true when fetch runs (not necessarily successfully)
-        static raw = fallback ? fallback : "";
+    const dynamicExtend = class DYNAMICEX extends extendTagClass {
+        static fetchRan = false;    // Updates to true when fetch runs (not necessarily successfully)
+        static raw = fallback;
 
         constructor() {
             super();
@@ -31,6 +31,7 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
 
             if (!this.scriptsRan) {
                 scripts.forEach(script => new Function("instance", "state", script.textContent)(this, this.state))        // TODO: error handling?
+                
                 styles.forEach(style => document.body.appendChild(style.cloneNode(true)))
             }
 
@@ -40,12 +41,13 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
             this.scriptsRan = dynamicExtend.fetchRan && true;
 
 
-            // Preprocess HTML string contents and append to dom
+            // Preprocess HTML string contents (so that {expressions} are evaluated and displayed properly) and append to dom
             (DOM.body.childNodes).forEach(n => {
-                if (n.nodeType == Node.ELEMENT_NODE)
-                    n.innerHTML = n.innerHTML.replace(/\{([^}]+)\}/g, (_, expr) => {    // TODO: error handling?
+                if (n.nodeType == Node.ELEMENT_NODE) {
+                    n.innerHTML = n.innerHTML.replace(/\{([^}]+)\}/g, (_, expr) => {                                        // TODO: error handling?
                         return new Function("instance", "state", `return (${expr})`)(this, this.state);
                     });
+                }
 
                 this.appendChild(n.cloneNode(true));
             });
@@ -55,9 +57,11 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
             this.onRerender.call(this);
         }
         
+        
         connectedCallback() {
             this.rerender();
         }
+
 
         disconnectedCallback() {
             this.onDestroy.call(this);
@@ -70,20 +74,21 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
         dynamicExtend.raw = raw;
         dynamicExtend.fetchRan = true;
         
-        if (extendTagName === null)
+        if (extendTagName === null) {
             customElements.define(name, dynamicExtend);
-        else 
+        } else { 
             customElements.define(name, dynamicExtend, { extends: extendTagName });
+        }
     };
 
 
     // Creating the customElement 
-    if (path != null) 
+    if (path != null) {
         fetch(path)
             .then(res => {              // Fetch content
-                if (res.ok)
+                if (res.ok) {
                     return res.text();
-                else {
+                } else {
                     throw new Error(`Status from component ${name} at location ${path} threw ${res.status}`);
                     return null;
                 }
@@ -91,10 +96,11 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
                 if (raw == null)    
                     return;
                 define(raw);            // Define the custom element using the helper function
-            }).catch(() => define(fallback))
-    else { 
-        define(fallback);
+            }).catch(() => define(fallback))    // Fallback content used if fetch fails
+    } else { 
+        define(fallback);                       // Fallback content used if path isn't provided
     }
+
 
     // Async fetch breaks component display - manually upgrade to return display to normal
     return customElements.whenDefined(name).then(() => {
