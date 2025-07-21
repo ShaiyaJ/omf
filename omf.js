6c92ff30610c8b9ce0a5766c1a7f9f17a00f7df5
Snishi <93013772+ShaiyaJ@users.noreply.github.com>
2025-07-21 15:56:57 +0100

    size optimisations
    inlined functions that are only used once

--- omf.js
+++ omf.js
@@ -1,29 +1,8 @@
-const entityDecode = document.createElement("textarea");
-
-function entityEval(s) {
-    entityDecode.innerHTML = s;
-    return entityDecode.value;
-}
-
-function filterComment(raw) {
-    const DOM = new DOMParser().parseFromString(raw, "text/html"); 
-    
-    const walker = document.createTreeWalker(DOM.body, NodeFilter.SHOW_COMMENT); 
-    let node;
-    let commentNodes = [];
-            
-    while (node = walker.nextNode()) {
-        commentNodes.push(node);
-    }
-            
-    commentNodes.forEach(n => n.remove());
-    
-    return DOM.body.innerHTML;
-}
+const e = document.createElement("textarea");
 
 function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
     // Get tag type from extendTagName
-    let extendTagClass = extendTagName === null ? HTMLElement : document.createElement(extendTagName).constructor;
+    let extendTagClass = extendTagName ? document.createElement(extendTagName).constructor : HTMLElement;
 
     // Create dynamic object
     const dynamicExtend = class DYNAMICEX extends extendTagClass {
@@ -32,7 +11,7 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
 
         constructor() {
             super();
-            this.raw = dynamicExtend.raw;
+            this.raw;
 
             this.state = {};
             this.onRerender = () => {};
@@ -49,13 +28,15 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
             const DOM = new DOMParser().parseFromString(this.raw, "text/html");
 
 
-            // Preprocess HTML string contents (so that {expressions} are evaluated and displayed properly) and append to dom 
+            // Preprocess HTML string contents (so that ${expressions} are evaluated and displayed properly) and append to dom 
+            e.innerHTML = DOM.body.innerHTML;                   // Decoding HTML entities so that javascript can execute correctly
+            
             try {
-                DOM.body.innerHTML = this.templateEval(entityEval(DOM.body.innerHTML));
-            } catch (e) {
-                console.log(entityEval(DOM.body.innerHTML));
-                throw e;
+                DOM.body.innerHTML = this.templateEval(e.value);
+            } catch (err) {
+                throw new Error(`${e.value}\n${err}`);
             }
+            
             (DOM.body.childNodes).forEach(n => this.appendChild(n.cloneNode(true)));
 
 
@@ -65,17 +46,14 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
         
         
         connectedCallback() {
-            const DOM = new DOMParser().parseFromString(this.raw, "text/html");
+            const DOM = new DOMParser().parseFromString(dynamicExtend.raw, "text/html");
             
             // Extract and run scripts and styles
             const scripts = DOM.querySelectorAll("script");
             const styles = DOM.querySelectorAll("style");
 
-            if (!this.scriptsRan) {
-                scripts.forEach(script => new Function("instance", "state", script.textContent)(this, this.state));        // TODO: error handling?
-                    
-                styles.forEach(style => document.body.appendChild(style.cloneNode(true)));
-            }
+            scripts.forEach(script => new Function("instance", "state", script.textContent)(this, this.state));        // TODO: error handling?
+            styles.forEach(style => document.body.appendChild(style.cloneNode(true)));
 
             scripts.forEach(s => s.remove());
             styles.forEach(s => s.remove());
@@ -94,7 +72,30 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
 
     // Define element helper function
     const define = (raw) => {
-        dynamicExtend.raw = filterComment(raw);
+        // Filter out comments
+        const DOM = new DOMParser().parseFromString(raw, "text/html"); 
+        
+        const walker = document.createTreeWalker(DOM.body, NodeFilter.SHOW_COMMENT); 
+        let node;
+        let commentNodes = [];
+                
+        while (node = walker.nextNode()) {
+            commentNodes.push(node);
+        }
+            
+        commentNodes.forEach(n => n.remove());
+        
+        // Potential optimisation by doing something like (this may cause issues with treewalker losing it's cursor)
+        /*
+        let node = walker.nextNode();
+        do {
+            node.remove();
+            node = walker.currentNode;
+        } while (walker.nextNode());
+        */
+        
+        // Setting element text and registering
+        dynamicExtend.raw = DOM.body.innerHTML;
         dynamicExtend.fetchRan = true;
         
         if (extendTagName === null) {
@@ -106,20 +107,11 @@ function component(name, path, {extendTagName = null, fallback = ""} = {} ) {
 
 
     // Creating the customElement 
-    if (path != null) {
+    if (path) {
         fetch(path)
-            .then(res => {              // Fetch content
-                if (res.ok) {
-                    return res.text();
-                } else {
-                    throw new Error(`Status from component ${name} at location ${path} threw ${res.status}`);
-                    return null;
-                }
-            }).then(raw => {            // Retroactively add content to DYNEX 
-                if (raw == null)    
-                    return;
-                define(raw);            // Define the custom element using the helper function
-            }).catch(() => define(fallback))    // Fallback content used if fetch fails
+            .then(res => res.ok ? res.text() : Promise.reject(new Error(`Status from component ${name} at location ${path} threw ${res.status}`)))
+            .then(raw => define(raw))           // Retroactively add content to DYNEX 
+            .catch(_ => define(fallback))       // Fallback content used if fetch fails
     } else { 
         define(fallback);                       // Fallback content used if path isn't provided
     }
